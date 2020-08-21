e0901b587922d9c9c617dc000fbb9f472bccc329
Ashisoma <okalionesmus@gmail.com>
2020-08-21 19:01:38 +0300

    add variables

---
+++ script.js
@@ -0,0 +1,11 @@
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday" ,"Friday" ,"Saturday"];
+var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame" ];
+
+var index;
+
+if (day == 0){
+    index = 6;
+}else {
+    index = day - 1;
+}
+console.log(index);
